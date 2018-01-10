d32eb2d0a5def77ec5fdeb7b3b40aa8c6044eef1
Forest Dussault <forest.dussault@inspection.gc.ca>
2018-01-10 11:26:47 -0500

    Improved docstrings

--- api.py
+++ api.py
@@ -81,20 +81,21 @@ def retrieve_issue_description(issue):
 def pickle_redmine(redmine_instance, issue, work_dir, description):
     """
     Function to pickle our redmine instance and issue
-    :param redmine_instance:
-    :param issue:
-    :param work_dir:
-    :return: dictionary with paths to redmine instance, issue and issue description pickles
+    :param redmine_instance: instantiated Redmine API object
+    :param issue: object pulled from Redmine instance
+    :param work_dir: string path to working directory for Redmine job
+    :return: dictionary with paths to redmine instance, issue and description pickles
     """
-    pickled_redmine = os.path.join(work_dir,'redmine.p')
-    pickled_issue = os.path.join(work_dir,'issue.p')
-    pickled_description = os.path.join(work_dir,'description.p')
+    pickled_redmine = os.path.join(work_dir,'redmine.pickle')
+    pickled_issue = os.path.join(work_dir,'issue.pickle')
+    pickled_description = os.path.join(work_dir,'description.pickle')
 
     # Create dictionary
     pickles = {'redmine_instance' : pickled_redmine,
                'issue' : pickled_issue,
                'description' : pickled_description}
 
+    # Write pickle files
     with open(pickled_redmine, 'wb') as file:
         pickle.dump(redmine_instance, file)
     with open(pickled_issue, 'wb') as file:
@@ -117,12 +118,13 @@ def make_executable(path):
 
 def create_template(issue, cpu_count, memory, work_dir, cmd):
     """
-    :param issue:
-    :param cpu_count:
-    :param memory:
-    :param work_dir:
-    :param cmd:
-    :return:
+    Creates a slurm job shell script
+    :param issue: object pulled from Redmine instance
+    :param cpu_count: number of CPUs to allocate for slurm job
+    :param memory: memory in MB to allocate for slurm job
+    :param work_dir: string path to working directory for Redmine job
+    :param cmd: string containing bash command
+    :return: string file path to generated shell script
     """
     template = "#!/bin/bash\n" \
                "#SBATCH -N 1\n" \
@@ -152,16 +154,25 @@ def create_template(issue, cpu_count, memory, work_dir, cmd):
     return file_path
 
 
-def submit_slurm_job(redmine_instance, resource_id, job, work_dir, cmd, cpu_count=8, memory=12000):
+def submit_slurm_job(redmine_instance, resource_id, issue, work_dir, cmd, cpu_count=8, memory=12000):
+    """
+    :param redmine_instance: instantiated Redmine API object
+    :param resource_id: ID pulled from issue instance
+    :param issue: object pulled from Redmine instance
+    :param work_dir: string path to working directory for Redmine job
+    :param cmd: string containing bash command
+    :param cpu_count: number of CPUs to allocate for slurm job
+    :param memory: memory in MB to allocate for slurm job
+    """
     # Set status of issue to In Progress
-    redmine_instance.issue.update(resource_id=job.id, status_id=2)
-    logging.info('Updated job status for {} to In Progress'.format(job.id))
+    redmine_instance.issue.update(resource_id=issue.id, status_id=2)
+    logging.info('Updated job status for {} to In Progress'.format(issue.id))
 
     # Create shell script
-    slurm_template = create_template(issue=job, cpu_count=cpu_count, memory=memory, work_dir=work_dir, cmd=cmd)
+    slurm_template = create_template(issue=issue, cpu_count=cpu_count, memory=memory, work_dir=work_dir, cmd=cmd)
 
     # Submit job to slurm
-    logging.info('Submitting job {} to Slurm'.format(job.id))
+    logging.info('Submitting job {} to Slurm'.format(issue.id))
     os.system('sbatch ' + slurm_template)
 
 
@@ -179,22 +190,29 @@ def main():
 
         # Queue up a slurm job for each new issue
         for job in new_jobs:
+            # Grab work directory
             work_dir = bio_requests_setup(job)
+
+            # Pull issue details from Redmine and dump to text file
             issue_text_dump(job)
+
+            # Pull issue description
             description = retrieve_issue_description(job)
 
+            # Pickle objects for usage by analysis scripts
             pickles = pickle_redmine(redmine_instance=redmine,
                                      issue=job,
                                      work_dir=work_dir,
                                      description=description)
 
-            #########################################
-            # Plug in your new Redmine scripts here #
-            #########################################
+            #############################################
+            ### Plug in your new Redmine scripts here ###
+            #############################################
 
-            if job.subject.lower() == 'strainmash_test':
+            if job.subject.lower() == 'strainmash':
                 logging.info('Detected STRAINMASH job for Redmine issue {}'.format(job.id))
 
+                # Prepare command to call analysis script with pickled Redmine objects
                 cmd = 'python ' \
                       '/mnt/nas/Redmine/OLCRedmineAutomator/automators/strainmash.py ' \
                       '--redmine_instance {redmine_pickle} ' \
@@ -202,26 +220,52 @@ def main():
                       '--work_dir {work_dir} ' \
                       '--description {description_pickle}'.format(redmine_pickle=pickles['redmine_instance'],
                                                                   issue_pickle=pickles['issue'],
-                                                                  work_dir=work_dir,
-                                                                  description_pickle=pickles['description'])
+                                                                  description_pickle=pickles['description'],
+                                                                  work_dir=work_dir,)
 
+                # Submit job to slurm
                 submit_slurm_job(redmine_instance=redmine,
                                  resource_id=job.id,
-                                 job=job,
+                                 issue=job,
                                  work_dir=work_dir,
                                  cmd=cmd,
                                  cpu_count=8,
                                  memory=12000)
 
-            elif job.subject == 'test2':
-                pass
-            else:
-                pass
+            elif job.subject.lower() == 'wgs assembly':
+                logging.info('Detected WGS ASSEMBLY job for Redmine issue {}'.format(job.id))
 
-            #########################################
+            elif job.subject.lower() == 'autoclark':
+                logging.info('Detected AUTOCLARK job for Redmine issue {}'.format(job.id))
 
-        # Take a nap for a minute
-        time.sleep(60)
+            elif job.subject.lower() == 'snvphyl':
+                logging.info('Detected SNVPHYL job for Redmine issue {}'.format(job.id))
+
+            elif job.subject.lower() == 'diversitree':
+                logging.info('Detected DIVIERSITREE job for Redmine issue {}'.format(job.id))
+
+            elif job.subject.lower() == 'irida retrieve':
+                logging.info('Detected IRIDA RETRIEVE job for Redmine issue {}'.format(job.id))
+
+            elif job.subject.lower() == 'resfindr':
+                logging.info('Detected RESFINDER job for Redmine issue {}'.format(job.id))
+
+            elif job.subject.lower() == 'external retrieve':
+                logging.info('Detected EXTERNAL RETRIEVE job for Redmine issue {}'.format(job.id))
+
+            elif job.subject.lower() == 'autoroga':
+                logging.info('Detected AUTOROGA job for Redmine issue {}'.format(job.id))
+
+            elif job.subject.lower() == 'retrieve':
+                logging.info('Detected RETRIEVE job for Redmine issue {}'.format(job.id))
+
+            #############################################
+            #############################################
+            #############################################
+
+        # Take a nap for 5 minutes
+        logging.info('A new scan for issues will be performed in 5 minutes')
+        time.sleep(300)
 
 
 if __name__ == '__main__':
