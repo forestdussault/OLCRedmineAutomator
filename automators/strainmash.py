4927f776f423235b17ea7852e8b1fec9cb8fbd6b
Forest Dussault <forest.dussault@inspection.gc.ca>
2018-01-31 15:10:45 -0500

    Improved taxonomy retrieval speed (local now), zipped file upload, and added file cleanup

--- automators/strainmash.py
+++ automators/strainmash.py
@@ -3,6 +3,7 @@ import re
 import glob
 import click
 import pickle
+import shutil
 import pandas as pd
 from Bio import Entrez
 from biotools import mash
@@ -44,24 +45,33 @@ def strainmash_redmine(redmine_instance, issue, work_dir, description):
     # Get all of the FASTA files
     fasta_list = glob.glob(os.path.join(work_dir, '*.fasta'))
 
-    # Output list containing dictionaries with file path as the key
-    output_list = []
-
     # Run mash_screen on everything
     for item in fasta_list:
         output_filepath = os.path.join(output_dir, (item.replace('.fasta', '') + '_strainmash.txt'))
 
-        # Setup dictionary for upload to Redmine
-        output_list.append(
-            {
-                'path': output_filepath,
-                'filename': os.path.basename(output_filepath)
-            }
-        )
-
         mash_screen(reference=typestrain_db_path,
                     queryfile=item,
                     outname=output_filepath)
+
+
+    # Move all files to the actual output folder - this is a workaround to a weird bug
+    output_files = glob.glob(os.path.join(work_dir, '*strainmash.txt'))
+    for file in output_files:
+        os.rename(file, os.path.join(output_dir, os.path.basename(file)))
+
+    # Zip output folder
+    shutil.make_archive(output_dir, 'zip', work_dir, 'output')
+
+    # Glob zip
+    zip_file = glob.glob(os.path.join(work_dir, '*.zip'))[0]
+
+    # Output list containing dictionaries with file path as the key
+    output_list = [
+        {
+            'path': os.path.join(work_dir, zip_file),
+            'filename': os.path.basename(zip_file)
+        }
+    ]
 
     # Upload files, set status to Feedback
     redmine_instance.issue.update(resource_id=issue.id, uploads=output_list, status_id=4)
@@ -69,6 +79,9 @@ def strainmash_redmine(redmine_instance, issue, work_dir, description):
     # Delete all of the FASTA files
     for fasta in fasta_list:
         os.remove(fasta)
+
+    # Delete the output folder
+    shutil.rmtree(output_dir)
 
 
 def mash_screen(reference, queryfile, outname):
@@ -99,9 +112,7 @@ def mash2pandas(outname):
     df = df[:10]
 
     # Map the extract_entrez_data function on each accession
-    # df['Organism'] = df[4].map(extract_species) # TODO: use local copy of DB so pinging NCBI isn't necessary
-    df['Organism'] = df[4].map(extract_entrez_data)
-
+    df['Organism'] = df[4].map(extract_species)
 
     # Print out top hit
     tophit_name = df[4].iloc[0]
@@ -126,56 +137,20 @@ def mash2pandas(outname):
         df.to_csv(outname, sep='\t', header=None, index=False)
 
 
-def extract_entrez_data(accession):
-    """
-    :param accession: GenBank assembly accession ID
-    :return: organism name stored in NCBI
-    """
-    Entrez.email = 'forest.dussault@inspection.gc.ca'
-
-    try:
-        handle = Entrez.esearch(db='assembly', term=accession)
-        record = Entrez.read(handle)
-    except:
-        pass
-
-    for id in record['IdList']:
-        # Get Assembly Summary
-        print('\nWorking on {}...'.format(id))
-        try:
-            esummary_handle = Entrez.esummary(db="assembly", id=id, report="full")
-            esummary_record = Entrez.read(esummary_handle)
-
-            # Parse Accession Name from Summary
-            accession_id = esummary_record['DocumentSummarySet']['DocumentSummary'][0]['AssemblyAccession']
-
-            # Search Name for ID
-            refseq_id = Entrez.read(Entrez.esearch(db="nucleotide", term=accession_id))['IdList'][0]
-
-            # Fetch Record with ID from nucleotide db
-            seq_record = Entrez.efetch(db="nucleotide", id=refseq_id, retmode='xml')
-
-            record = Entrez.read(seq_record)
-            organism = record[0]['GBSeq_organism']
-        except:
-            print('Failed')
-            return None
-
-        # Might want to implement these later
-        primary_accession = record[0]['GBSeq_primary-accession']
-        taxonomy = record[0]['GBSeq_taxonomy']
-
-        print(organism)
-        return organism
-
-
 def extract_species(accession):
     try:
-        df = pd.read_csv('/mnt/nas/Databases/RefSeq/accession_taxonomy_relationships.csv')
+        df = pd.read_csv('/mnt/nas/Databases/RefSeq/assembly_summary_refseq.txt',
+                         skiprows=1,
+                         delimiter='\t')
         df_filtered = df[df.values == accession]
         organism = df_filtered['organism_name'].values[0]
+
+        # Consistency
+        if organism is 'na':
+            organism = 'N/A'
     except:
         organism = 'N/A'
+
     return organism
 
 
