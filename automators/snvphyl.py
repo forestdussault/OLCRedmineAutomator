441a72af4b1d9caf408e66197a8cbacee0408f41
Andrew Low <andrew.low@inspection.gc.ca>
2018-04-09 09:24:13 -0400

    Snvphyl ref file upload appears to be working. Adding to regular automator

--- automators/snvphyl.py
+++ automators/snvphyl.py
@@ -38,25 +38,47 @@ def snvphyl_redmine(redmine_instance, issue, work_dir, description):
         # Retrieve our reference file. Error user if they selected anything but one reference and don't continue.
         if len(reference) != 1:
             redmine_instance.issue.update(resource_id=issue.id,
-                                          notes='Error! You must specify one reference strain, and you '
+                                          notes='ERROR: You must specify one reference strain, and you '
                                                 'specified {} reference strains. Please create a new'
                                                 ' issue and try again.'.format(len(reference)), status_id=4)
             return
 
-        # Extract our reference file to our working directory.
-        with open(os.path.join(work_dir, 'seqid.txt'), 'w') as f:
-            f.write(reference[0])
+        if reference[0].upper() != 'ATTACHED':
+            # Extract our reference file to our working directory.
+            with open(os.path.join(work_dir, 'seqid.txt'), 'w') as f:
+                f.write(reference[0])
 
-        cmd = 'python2 /mnt/nas/WGSspades/file_extractor.py {}/seqid.txt {} /mnt/nas/'.format(work_dir, work_dir)
-        os.system(cmd)
+            cmd = 'python2 /mnt/nas/WGSspades/file_extractor.py {}/seqid.txt {} /mnt/nas/'.format(work_dir, work_dir)
+            os.system(cmd)
 
-        # Check that the file was successfully extracted. If it wasn't boot the user.
-        if len(glob.glob(os.path.join(work_dir, '*fasta'))) == 0:
-            redmine_instance.issue.update(resource_id=issue.id,
-                                          notes='Error! Could not find the specified reference file.'
-                                                ' Please verify it is a correct SEQID, create a new '
-                                                'issue, and try again.', status_id=4)
-            return
+            # Check that the file was successfully extracted. If it wasn't boot the user.
+            if len(glob.glob(os.path.join(work_dir, '*fasta'))) == 0:
+                redmine_instance.issue.update(resource_id=issue.id,
+                                              notes='ERROR: Could not find the specified reference file.'
+                                                    ' Please verify it is a correct SEQID, create a new '
+                                                    'issue, and try again.', status_id=4)
+                return
+
+        # If user specified attachment as the reference file, download it to our working directory.
+        else:
+            # Get the attachment ID, and download if it isn't equal to zero (meaning no attachment, so boot user with
+            # appropriate error message)
+            attachment = redmine_instance.issue.get(issue.id, include='attachments')
+            attachment_id = 0
+            for item in attachment.attachments:
+                attachment_id = item.id
+
+            # Download if we found an attachment, and use as our reference. Otherwise, exit and tell user to try again
+            if attachment_id != 0:
+                attachment = redmine_instance.attachment.get(attachment_id)
+                attachment.download(savepath=work_dir, filename='reference.fasta')
+            else:
+                redmine_instance.issue.update(resource_id=issue.id,
+                                              notes='ERROR: You specified that the reference would be in attached file,'
+                                                    ' but no attached file was found. Please create a new issue and '
+                                                    'try again.',
+                                              status_id=4)
+                return
 
         # Now extract our query files.
         with open(os.path.join(work_dir, 'seqid.txt'), 'w') as f:
@@ -143,6 +165,7 @@ def verify_fastqs_present(query_list, fastq_folder):
             missing_fastqs.append(query)
     # Returns list of SEQIDs for which we couldn't find forward and/or reverse reads
     return missing_fastqs
+
 
 if __name__ == '__main__':
     snvphyl_redmine()
